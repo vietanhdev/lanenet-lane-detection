87eb47afe2dd650f6713725e5eaa804e52794fe2
Viet Anh Nguyen <vietanh@vietanhdev.com>
2020-03-17 20:35:31 +0700

    Add testing and model exporting code

---
+++ tools/test_lanenet_fps.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 18-5-23 ??11:33
+# @Author  : MaybeShewill-CV
+# @Site    : https://github.com/MaybeShewill-CV/lanenet-lane-detection
+# @File    : test_lanenet.py
+# @IDE: PyCharm Community Edition
+"""
+test LaneNet model on single image
+"""
+import argparse
+import os.path as ops
+import time
+
+import cv2
+import glog as log
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from config import global_config
+from lanenet_model import lanenet
+from lanenet_model import lanenet_postprocess
+
+from tensorflow.python.framework import graph_util
+from tensorflow.python.framework import graph_io
+
+CFG = global_config.cfg
+
+
+def init_args():
+    """
+
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_path', type=str, help='The image path or the src image save dir')
+    parser.add_argument('--weights_path', type=str, help='The model weights path')
+
+    return parser.parse_args()
+
+
+def args_str2bool(arg_value):
+    """
+
+    :param arg_value:
+    :return:
+    """
+    if arg_value.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+
+    elif arg_value.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Unsupported value encountered.')
+
+
+def minmax_scale(input_arr):
+    """
+
+    :param input_arr:
+    :return:
+    """
+    min_val = np.min(input_arr)
+    max_val = np.max(input_arr)
+
+    output_arr = (input_arr - min_val) * 255.0 / (max_val - min_val)
+
+    return output_arr
+
+
+def test_lanenet(image_path, weights_path):
+    """
+
+    :param image_path:
+    :param weights_path:
+    :return:
+    """
+    assert ops.exists(image_path), '{:s} not exist'.format(image_path)
+
+    log.info('Start reading image and preprocessing')
+    t_start = time.time()
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    image_vis = image
+    image = cv2.resize(image, (512, 256), interpolation=cv2.INTER_LINEAR)
+    image = image / 127.5 - 1.0
+    log.info('Image load complete, cost time: {:.5f}s'.format(time.time() - t_start))
+
+    input_tensor = tf.placeholder(dtype=tf.float32, shape=[1, 256, 512, 3], name='input_tensor')
+
+    net = lanenet.LaneNet(phase='test', net_flag='vgg')
+
+    n_images = 0
+    total_runtime = 0
+    for i in range(1000):
+        start_time = time.time()
+        binary_seg_ret, instance_seg_ret = net.inference(input_tensor=input_tensor, name='lanenet_model')
+        # mask = binary_seg_ret[0]
+        # mask *= 255
+        # mask = mask.astype(np.uint8)
+        run_time = time.time() - start_time
+        total_runtime += run_time
+        n_images += 1
+
+        avg_time = total_runtime / n_images
+        print("Avg time: " + str(avg_time) + " - FPS: " + str(1 / avg_time))
+
+    return
+
+
+if __name__ == '__main__':
+    """
+    test code
+    """
+    # init args
+    args = init_args()
+
+    test_lanenet(args.image_path, args.weights_path)
